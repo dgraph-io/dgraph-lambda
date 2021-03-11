b0f77bef9fff4755e0d40146fc211c74cf4488c6
vardhanapoorv <vardhanapoorv@gmail.com>
2021-03-11 13:00:42 +0530

    Add support for webhooks

--- slash-graphql-lambda-types/types.d.ts
+++ slash-graphql-lambda-types/types.d.ts
@@ -9,11 +9,43 @@ declare module "@slash-graphql/lambda-types" {
     value: string | undefined
   }
 
+  type eventPayload = {
+    __typename: string,
+    operation: string,
+    commitTs: number,
+    add: addEvent | undefined,
+    update: updateEvent | undefined,
+    delete: deleteEvent | undefined
+  }
+
+
+  type addEvent = {
+    add: {
+      rootUIDs: Array<any>,  
+      input: Array<any>
+    } 
+  }
+
+  type updateEvent = {
+    update: {
+      rootUIDs: Array<any>,
+      SetPatch: Object,
+      RemovePatch: Object
+    } 
+  }
+
+  type deleteEvent = {
+    delete: {
+      rootUIDs: Array<any>
+    } 
+  }
+
   type GraphQLEventFields = {
     type: string,
     parents: (Record<string, any>)[] | null,
     args: Record<string, any>,
-    authHeader?: AuthHeaderField
+    authHeader?: AuthHeaderField,
+    event?: eventPayload
   }
 
   type ResolverResponse = any[] | Promise<any>[] | Promise<any[]>;
@@ -27,12 +59,27 @@ declare module "@slash-graphql/lambda-types" {
     },
   }
 
+  type WebHookGraphQLEvent = {
+    respondWith: (r: ResolverResponse) => void,
+    graphql: (s: string, vars: Record<string, any> | undefined, ah?: AuthHeaderField) => Promise<GraphQLResponse>,
+    dql: {
+      query: (s: string, vars: Record<string, any> | undefined) => Promise<GraphQLResponse>
+      mutate: (s: string) => Promise<GraphQLResponse>
+    },
+    authHeader?: AuthHeaderField,
+    event?: eventPayload
+  } 
+
   type GraphQLEventWithParent = GraphQLEvent & {
     parent: Record<string, any> | null
   }
 
   function addGraphQLResolvers(resolvers: {
     [key: string]: (e: GraphQLEventWithParent) => any;
+  }): void
+
+  function addWebHookResolvers(resolvers: {
+    [key: string]: (e: WebHookGraphQLEvent) => any;
   }): void
 
   function addMultiParentGraphQLResolvers(resolvers: {
